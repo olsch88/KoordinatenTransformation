79fcbab10fffc7a794549874ea3e416ddde6d4be
olsch88 <olsch88@googlemail.com>
2024-03-22 13:25:01 +0100

    Projektdateien hinzufügen.

---
+++ KoordinatenTransformation/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System.Runtime.InteropServices;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+using MatrixLibrary;
+
+namespace KoordinatenTransformation;
+
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        double[,] source = { { -4.6579, 5.3374, 15.3328 }, { 0.9753, 1.2799, 1.5844 } };
+        double[,] target = { { 0, 10, 20 }, { 0, 0, 0 } };
+
+        Matrix<double> sourceMatrix = CoordinateTransformation.GetSourceMatrix(source);
+        Matrix<double> targetMatrix = CoordinateTransformation.GetSourceMatrix(target);
+
+        Matrix<double> transformation = CoordinateTransformation.GetTransofmationMatrix(sourceMatrix, targetMatrix);
+
+
+        Vector<double> example1 = Vector<double>.Build.Dense(3);
+        example1[0] = 25.3282;
+        example1[1] = 1.8889;
+        example1[2] = 1;
+
+
+        Vector<double> example1_target = CoordinateTransformation.applyTransformattion(transformation, example1);
+
+        Console.WriteLine(example1_target);
+
+        Vector<double> example2 = Vector<double>.Build.Dense(3);
+        example1[0] = 35.3235;
+        example1[1] = 2.1935;
+        example1[2] = 1;
+
+        Vector<double> example2_target = CoordinateTransformation.applyTransformattion(transformation, example1);
+
+        Console.WriteLine(example2_target);
+
+    }
+}
---
+++ MatrixLibrary/CoordinateTransformation.cs
@@ -0,0 +1,45 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace MatrixLibrary;
+
+public class CoordinateTransformation
+{
+    public static Matrix<double> GetSourceMatrix(double[,] numbers)
+    {
+        Matrix<double> A = DenseMatrix.OfArray(new double[,] {
+        {numbers[0,0],numbers[0,1],numbers[0,2]},
+        {numbers[1,0],numbers[1,1],numbers[1,2]},
+        {1,1,1}});
+
+
+
+        return A;
+    }
+
+    public static Matrix<double> GetTargetMatrix(double[,] numbers)
+    {
+        Matrix<double> A = DenseMatrix.OfArray(new double[,] {
+        {numbers[0,0],numbers[0,1],numbers[0,2]},
+        {numbers[1,0],numbers[1,1],numbers[1,2]},
+        {1,1,1}});
+
+
+
+        return A;
+    }
+
+    public static Matrix<double> GetTransofmationMatrix(Matrix<double> matrixSource, Matrix<double> matrixTarget)
+    {
+        Matrix<double> matrixTransformation = matrixTarget.Multiply( matrixSource.Inverse());
+
+        return matrixTransformation;
+
+    }
+
+    public static  Vector<double> applyTransformattion(Matrix<double> matrixTransformation, Vector<double> sourceVector)
+    {
+        Vector<double> result = matrixTransformation.Multiply(sourceVector);
+        return result;
+    }
+}
